c8c180691bed8e0b4148467e228b0f657a6bc4ca
tehAndrew <andpa_kontakt@live.se>
2019-02-06 17:03:21 +0100

    New class Scania.java added.

--- src/Scania.java
+++ src/Scania.java
@@ -1,10 +1,29 @@
 import java.awt.*;
 
 public class Scania extends Vehicle {
+    private double flatbedAngle;
 
     public Scania(){
+        super(2, 730, Color.blue, "Scania");
+        flatbedAngle = 0;
+    }
 
-        super(2, 125, Color.red, "Saab95");
+    public double getFlatbedAngle() {
+        return flatbedAngle;
+    }
+
+    public void raiseFlatbed(double angle) {
+        if (currentSpeed == 0) {
+            flatbedAngle = Math.min(flatbedAngle + angle, 70);
+        }
+    }
+
+    public void lowerFlatbed(double angle) {
+        flatbedAngle = Math.max(flatbedAngle - angle, 0);
+    }
+
+    public double speedFactor(){
+        return flatbedAngle > 0 ? 0 : enginePower * 0.01;
     }
 
     public void incrementSpeed(double amount){
